ca6f069ae0d38a7d056b298ab72ab7c9e9b3f608
Alvin Laia <alvinlaia2004@gmail.com>
2024-11-02 09:53:29 +0700

    Fix deployment issues

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://localhost:5000',
-    'http://127.0.0.1:5000'
+    'https://my-app-frontend-production-e401.up.railway.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
